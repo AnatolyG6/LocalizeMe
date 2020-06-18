291992ca43f8cd93e7aec3061c3c2de14eff39b6
Anatoly1996 <anatolygasiuk@gmail.com>
2020-06-18 22:11:30 +0200

    Update application component

--- app/src/main/java/com/test/localizeme/di/ApplicationComponent.java
+++ app/src/main/java/com/test/localizeme/di/ApplicationComponent.java
@@ -1,19 +1,20 @@
 package com.test.localizeme.di;
 
-import android.app.Application;
 import android.content.Context;
 
-import com.test.localizeme.MainActivity;
+import com.test.localizeme.app.currentspeed.MainActivity;
 
 import javax.inject.Singleton;
 
-import dagger.BindsInstance;
 import dagger.Component;
-import dagger.android.DaggerApplication;
 
 @Singleton
-@Component(modules = {RepositoryModule.class})
-interface ApplicationComponent {
+@Component(modules = {
+        MainActivityModule.class,
+        RepositoryModule.class,
+        LocationModule.class,
+})
+public interface ApplicationComponent {
     Context context();
 
     void inject(MainActivity main);
